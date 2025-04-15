0a057d041b6c37b2084ce783ad53fff65ea8eccb
gubanova.av <mrs.pohyi@gmail.com>
2025-04-15 09:45:02 +0300

    added resources added working test

--- src/test/java/AutomationPracticeFormTest.java
+++ src/test/java/AutomationPracticeFormTest.java
@@ -3,9 +3,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.by;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class AutomationPracticeFormTest {
 
@@ -21,28 +19,51 @@ public class AutomationPracticeFormTest {
     @Test
     void FormTest() {
         open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
 
         // Заполнение формы
         $("#firstName").setValue("Nastia");
         $("#lastName").setValue("Gubanova");
-        $("#userEmail").setValue("test@nastia.ru"); //Выбор пола
-        $("label[class=custom-control-label]").click();
+        $("#userEmail").setValue("test@nastia.ru");
+        $("label[for=gender-radio-2]").click(); //Выбор пола
         $("#userNumber").setValue("1234567890"); // Номер телефона
         $("[id=dateOfBirthInput]").click(); // Выбор даты рождения
-        $("[class=react-datepicker__year-select]>[value='2000']").click();
-        $("[class=react-datepicker__month-select]>[value='3']").click();
-        $(".react-datepicker__month .react-datepicker__day--024").click();
+        $("[class=react-datepicker__year-select]>[value='1989']").click();
+        $("[class=react-datepicker__month-select]>[value='4']").click();
+        $(".react-datepicker__month .react-datepicker__day--020").click();
         $("[id=subjectsContainer]").click();
-        $("#dateOfBirthInput").setValue("20 May 1989"); // Дата рождения
-        $("#hobbies-checkbox-1").click(); // Выбор хобби
-        $("#currentAddress").setValue("Some street 1"); // Текущий адрес
-
+        $("[id=subjectsInput]").setValue("Maths").pressEnter();
+        $("#hobbiesWrapper > div.col-md-9.col-sm-12 > div:nth-child(1) > label").click();
+        $("[id=uploadPicture]").uploadFromClasspath("egg.jpg"); //Загрузка картинки
+        $("#currentAddress").setValue("Monkey 23"); // Текущий адрес
+        $("[id=react-select-3-input]").setValue("NCR").pressEnter();
+        $("[id=react-select-4-input]").setValue("Delhi").pressEnter();
         // Отправка формы
         $("#submit").click();
 
         // Проверка результатов
-        $("#output #name").shouldHave(text("Nastia Gubanova"));
-        $("#output #email").shouldHave(text("test@nastia.ru"));
-        $("#output #currentAddress").shouldHave(text("Some street 1"));
+       // $("[class=table-responsive]").shouldHave(Condition.text("Nastia Gubanova"));
+       // $("[class=table-responsive]").shouldHave(Condition.text("test@nastia.ru"));
+      //  $("[class=table-responsive]").shouldHave(Condition.text("20 May,1989"));
+//$("[class=table-responsive]").shouldHave(Condition.text("Female"));
+//$("[class=table-responsive]").shouldHave(Condition.text("1234567890"));
+//$("[class=table-responsive]").shouldHave(Condition.text("Maths"));
+//$("[class=table-responsive]").shouldHave(Condition.text("Sports"));
+//$("[class=table-responsive]").shouldHave(Condition.text("egg.jpg"));
+//$("[class=table-responsive]").shouldHave(Condition.text("Monkey 23"));
+//$("[class=table-responsive]").shouldHave(Condition.text("NCR Delhi"));
+   //     $("#closeLargeModal").click();
+
+        $x("//td[contains(text(),'Student Name')]/../td[2]").shouldHave(text("Nastia Gubanova"));
+        $x("//td[contains(text(),'Student Email')]/../td[2]").shouldHave(text("test@nastia.ru"));
+        $x("//td[contains(text(),'Gender')]/../td[2]").shouldHave(text("Female"));
+        $x("//td[contains(text(),'Mobile')]/../td[2]").shouldHave(text("1234567890"));
+        $x("//td[contains(text(),'Date of Birth')]/../td[2]").shouldHave(text("20 May,1989"));
+        $x("//td[contains(text(),'Subjects')]/../td[2]").shouldHave(text("Maths"));
+        $x("//td[contains(text(),'Hobbies')]/../td[2]").shouldHave(text("Sports"));
+        $x("//td[contains(text(),'Picture')]/../td[2]").shouldHave(text("egg.jpg"));
+        $x("//td[contains(text(),'Address')]/../td[2]").shouldHave(text("Monkey 23"));
+        $x("//td[contains(text(),'State and City')]/../td[2]").shouldHave(text("NCR Delhi"));
     }
 }
