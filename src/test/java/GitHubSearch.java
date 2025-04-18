397a9644897bcb3691b302ff770e7f62094aaaea
gubanova.av <mrs.pohyi@gmail.com>
2025-04-18 11:11:31 +0300

    added git test

---
+++ src/test/java/GitHubSearch.java
@@ -0,0 +1,36 @@
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GitHubSearch {
+    @Test
+    void ShouldFindJunit5Code () {
+        //Открываем главную страницу
+        open("https://github.com/");
+        // Кликаем на поле поиска, чтобы активировать его
+        $(".header-search-button").click();
+        // Ввод в поле поиска "selenide"
+        $("input[id='query-builder-test']").setValue("selenide").pressEnter();
+        //Открываем раздел selenide
+        $("a[href='/selenide/selenide']").click();
+        //Открываем раздел wiki
+        $("[id='wiki-tab']").click();
+        //Открывает SoftAssertions
+        $("a[href='/selenide/selenide/wiki/SoftAssertions']").click();
+        //Проверка кода
+        $$("h4.heading-element").findBy(text("JUnit5")).should(exist);
+        $("#wiki-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class}) " +
+                "class Tests { " +
+                "  @Test " +
+                "  void test() { " +
+                "    Configuration.assertionMode = SOFT; " +
+                "    open(\"page.html\"); " +
+                "    $(\"#first\").should(visible).click(); " +
+                "    $(\"#second\").should(visible).click(); " +
+                "  } " +
+                "}"));
+
+    }
+}
